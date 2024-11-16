935bb219c4a098692daed3459fea8ea91eda75e4
Hana Kobori <hakobori@c2r8s6.42tokyo.jp>
2024-11-16 17:24:08 +0900

    added checkers and gnl

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   cub3D.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/16 15:35:44 by hakobori          #+#    #+#             */
-/*   Updated: 2024/11/16 15:35:48 by hakobori         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef CUB3D_H
 #define CUB3D_H
 
@@ -17,8 +5,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include"./libs/libft/libft.h"
+#include"./libs/get_next_line/get_next_line.h"
 
 #define TRUE 1
 #define FALSE 0
+
+typedef struct s_map {
+	char	*no;
+	char	*so;
+	char	*we;
+	char	*ea;
+	char	*f;
+	int		width;
+	int		height;
+	char	**structure;
+}			t_map;
+
 
 #endif
