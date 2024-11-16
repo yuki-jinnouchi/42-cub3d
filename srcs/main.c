5783dcd0afe55b5e04edb85de298887efb5af3db
Yuki Jinnouchi <yjinnouc@c2r8s7.42tokyo.jp>
2024-11-16 17:28:13 +0900

    add main.c

--- srcs/main.c
+++ srcs/main.c
@@ -1,20 +1,23 @@
-/* ************************************************************************** */
+	/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:27:27 by hakobori          #+#    #+#             */
-/*   Updated: 2024/11/16 15:33:46 by hakobori         ###   ########.fr       */
+/*   Updated: 2024/11/16 17:10:21 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3D.h"
+#include "cub3d.h"
 
 int main(char argc, char **argv)
 {
 	if(arg_check(argv) == FALSE)
 		return (0);
-
+	if(map_check(argv) == FALSE)
+		return (0);
+	if(map_parse(argv) == FALSE)
+		return (0);
 }
