1e4fe9b14bec0d6dd3934ba7b341f1f2eb8d91ec
Hana Kobori <hakobori@c2r8s6.42tokyo.jp>
2024-11-16 15:39:58 +0900

    added cub3d.h, main.c, arg_check.c

---
+++ includes/cub3D.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3D.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 15:35:44 by hakobori          #+#    #+#             */
+/*   Updated: 2024/11/16 15:35:48 by hakobori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
