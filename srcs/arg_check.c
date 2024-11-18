8fdaae6daa79f580320688d839fc6f5b41849544
Hana Kobori <hakobori@c1r7s1.42tokyo.jp>
2024-11-18 22:45:26 +0900

    added map check

--- srcs/arg_check.c
+++ srcs/arg_check.c
@@ -6,15 +6,15 @@
 /*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:43:06 by hakobori          #+#    #+#             */
-/*   Updated: 2024/11/16 16:06:32 by hakobori         ###   ########.fr       */
+/*   Updated: 2024/11/17 21:03:50 by hakobori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int arg_check(int argc, char **argv)
+int	arg_check(int argc, char **argv)
 {
-	int arg_len;
+	int	arg_len;
 
 	arg_len = ft_strlen(argv[1]);
 	if (argc != 2)
@@ -27,11 +27,10 @@ int arg_check(int argc, char **argv)
 		printf("Error\nInvalid file extension\n");
 		return (FALSE);
 	}
-	if(ft_strncmp(argv[1] + arg_len - 4, ".cub", 4) != 0)
+	if (ft_strncmp(argv[1] + arg_len - 4, ".cub", 4) != 0)
 	{
 		printf("Error\nInvalid file extension\n");
 		return (FALSE);
 	}
 	return (TRUE);
 }
-
--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,19 @@
 /*   By: hakobori <hakobori@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 15:27:27 by hakobori          #+#    #+#             */
-/*   Updated: 2024/11/16 16:54:27 by hakobori         ###   ########.fr       */
+/*   Updated: 2024/11/17 20:33:24 by hakobori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-int main(char argc, char **argv)
+int	main(char argc, char **argv)
 {
-	t_map map_info;
-	t_map *map_info_ptr;
-	
+	t_map	map_info;
+	t_map	*map_info_ptr;
+
 	map_info_ptr = &map_info;
 	ft_bzero(map_info_ptr, sizeof(t_map));
-	if(map_check(argc, argv, map_info_ptr) == FALSE)
+	if (map_check(argc, argv, map_info_ptr) == FALSE)
 		return (0);
-
 }
