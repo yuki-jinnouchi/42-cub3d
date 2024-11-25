fa082e702bfb51c604398cd46bc37fe3acf8972e
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-11-25 07:02:22 +0000

    change folder name from sprite to texture

--- srcs/init/texture_init.c
+++ srcs/init/texture_init.c
@@ -6,7 +6,7 @@
 /*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 10:35:09 by yjinnouc          #+#    #+#             */
-/*   Updated: 2024/11/20 02:30:01 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2024/11/25 07:00:09 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int texture_init(t_vars *vars)
         return (FAILURE);
     vars->texture = texture;
     texture->tile_size = TILE_SIZE;
-    load_image(&texture->n, "./sprite/NO.xpm", vars);
-    load_image(&texture->s, "./sprite/SO.xpm", vars);
-    load_image(&texture->w, "./sprite/WE.xpm", vars);
-    load_image(&texture->e, "./sprite/EA.xpm", vars);
+    load_image(&texture->n, "./texture/NO.xpm", vars);
+    load_image(&texture->s, "./texture/SO.xpm", vars);
+    load_image(&texture->w, "./texture/WE.xpm", vars);
+    load_image(&texture->e, "./texture/EA.xpm", vars);
     texture->floor_argb = rgb_to_16argb(50, 30, 0);
     texture->ceil_argb = rgb_to_16argb(160, 240, 240);
 	return (SUCCESS);
