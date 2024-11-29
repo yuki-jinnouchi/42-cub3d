05adec45754d4a0bffc9b30089926d8631dd9202
Yuki JINI Jinnouchi <yuki.jinnouchi@gmail.com>
2024-11-29 08:11:06 +0000

    fix texture

--- srcs/init/init_texture.c
+++ srcs/init/init_texture.c
@@ -6,7 +6,7 @@
 /*   By: yjinnouc <yjinnouc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 10:35:09 by yjinnouc          #+#    #+#             */
-/*   Updated: 2024/11/29 08:01:18 by yjinnouc         ###   ########.fr       */
+/*   Updated: 2024/11/29 08:10:43 by yjinnouc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,14 @@ int texture_init(t_vars *vars)
     if (texture == NULL)
         return (FAILURE);
     vars->texture = texture;
-    load_image(&texture->n, "./texture/NO.xpm", vars);
-    load_image(&texture->s, "./texture/SO.xpm", vars);
-    load_image(&texture->w, "./texture/WE.xpm", vars);
-    load_image(&texture->e, "./texture/EA.xpm", vars);
+    if (load_image(&texture->n, "./texture/North.xpm", vars) == FAILURE)
+        return (FAILURE);
+    if (load_image(&texture->s, "./texture/South.xpm", vars) == FAILURE)
+        return (FAILURE);
+    if (load_image(&texture->w, "./texture/West.xpm", vars) == FAILURE)
+        return (FAILURE);
+    if (load_image(&texture->e, "./texture/East.xpm", vars) == FAILURE)
+        return (FAILURE);
     texture->floor_argb = rgb_to_16argb(50, 30, 0);
     texture->ceil_argb = rgb_to_16argb(160, 240, 240);
 	return (SUCCESS);
